626eec09739dd89c5f4ba26480faf1c79d33ea6b
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2021-11-07 10:27:11 +0100

    initali project

---
+++ src/main/java/pl/jkanclerz/creditcard/App.java
@@ -0,0 +1,10 @@
+package pl.jkanclerz.creditcard;
+
+import pl.jkanclerz.greetings.Greeter;
+
+public class App {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.hello("Kuba"));
+    }
+}
---
+++ src/main/java/pl/jkanclerz/greetings/Greeter.java
@@ -0,0 +1,7 @@
+package pl.jkanclerz.greetings;
+
+public class Greeter {
+    public String hello(String name) {
+        return String.format("Hello %s", name);
+    }
+}

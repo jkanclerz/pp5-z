64c4f0af96e943637292fba23b3f9418efbdfbb9
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2022-01-15 15:28:04 +0100

    add sample product at the begining

--- src/main/java/pl/kkanclerz/App.java
+++ src/main/java/pl/kkanclerz/App.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import pl.kkanclerz.greetings.Greeter;
 import pl.kkanclerz.productcatalog.*;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
@@ -21,7 +24,27 @@ public class App {
 
     @Bean
     ProductCatalog createCatalog(ProductStorage productStorage) {
-        return new ProductCatalog(productStorage);
+        ProductCatalog productCatalog = new ProductCatalog(productStorage);
+
+        productCatalog.addProduct(
+                "product-1",
+                "My Nice Picture",
+                "Very nice"
+        );
+
+        productCatalog.updatePrice("product-1", BigDecimal.valueOf(10.10));
+        productCatalog.publish("product-1");
+
+        productCatalog.addProduct(
+                "product-2",
+                "My Nice Picture 2",
+                "Bla bla"
+        );
+
+        productCatalog.updatePrice("product-2", BigDecimal.valueOf(20.10));
+        productCatalog.publish("product-2");
+
+        return productCatalog;
     }
 
     @Bean
